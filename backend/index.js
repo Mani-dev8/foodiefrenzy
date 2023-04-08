483b98a085f289fcfc58b976812ea02e54d7f759
manish <manishsewamahto090802@gmail.com>
2023-04-08 16:30:05 +0530

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ dotenv.config({path:'./.env'})
 const mongodb = require('./db');
 const mongoose = require("mongoose")
 const app = express()
+mongodb();
 const port = process.env.PORT
-
-const fetchFilterModel = mongoose.model('fetchFilter', {}, 'filterItems')
+const fetchFoodItemModel = mongoose.model('fetchData', {},'foodItems');
+const fetchCategoryModel=mongoose.model('fetchCategory',{},'Category');
+const fetchFilterModel = mongoose.model('fetchFilter', {}, 'filterItems');
 //middleware
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,12 +18,20 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.json())
-mongodb();
 app.get("/",(req,res)=>{
     res.send("welcome to foodiefrenzy")
 })
 app.use('/api/new', require('./Routes/userRoute'))
-app.use("/api", require('./Routes/cardData'))
+app.post("/api/cardData", async (req,res)=>{
+    try {
+        const foodItems = await fetchFoodItemModel.find({});
+        const foodCategory = await fetchCategoryModel.find({});
+        res.status(200).send([foodItems,foodCategory])
+    } catch (error) {
+        console.log("unable to find data from mongoDB ~~~~ ",error);
+        res.status(400).json({message:"Fialed to fetch data from MongoDB"});
+    }
+})
 app.post('/api/filterData', async (req,res)=>{
     try {
         const filterItems = await fetchFilterModel.find({});
