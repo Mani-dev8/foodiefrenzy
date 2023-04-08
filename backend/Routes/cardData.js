38fc5878018197c83354b6ec66c660f4308b873e
manish <manishsewamahto090802@gmail.com>
2023-04-08 09:43:38 +0530

    versel build changed

--- backend/Routes/cardData.js
+++ backend/Routes/cardData.js
@@ -2,7 +2,7 @@ const express=require("express");
 const route=express.Router()
     
 route.post("/cardData",(req,res)=>{
-    res.send([global.foodItems,global.catData])
+    res.json([global.foodItems,global.catData])
 })
 
 module.exports=route
